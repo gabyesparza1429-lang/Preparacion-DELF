beac6e30bd751352c1c6ca670515c70917dc5c85
Gabriela Esparza <gabyesparza1429@gmail.com>
2026-05-06 09:49:24 -0600

    Update Firebase imports to use CDN links

--- database.js
+++ database.js
@@ -1,5 +1,5 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore, doc, setDoc } from "firebase/firestore";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js";
+import { getFirestore, doc, setDoc } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-firestore.js";
 
 // 1. Configuración oficial de tu proyecto
 const firebaseConfig = {
